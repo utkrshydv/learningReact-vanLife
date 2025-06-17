24558774353dea94765e8dc938053256a7ab1fc8
utkrshydv <utkarsh4socials@gmail.com>
2025-06-17 20:52:07 +0530

    learn about browserRouter, routes, route and link

---
+++ src/Contact.jsx
@@ -0,0 +1,7 @@
+function Contact(){
+  return(
+    <h1>Contact me @gmail.com</h1>
+  )
+}
+
+export default Contact
---
+++ src/Home.jsx
@@ -0,0 +1,8 @@
+function Home(){
+  return (
+    <h1>Hello from Home Page</h1>
+  )
+}
+
+
+export default Home
